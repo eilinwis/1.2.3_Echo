062c9a4a191fffdd1372906c755764c71ef78c2a
Roman Kudryavtsev <TTe4kiH98@gmail.com>
2021-05-23 12:32:54 +0300

    code fixed

--- src/test/java/ru/netology/RequestTest.java
+++ src/test/java/ru/netology/RequestTest.java
@@ -19,6 +19,6 @@ class RequestTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data");
+                .body("data", equalTo("some data"));
     }
 }
